d147c3865b0ade990eee4c0cbede2c8445d5d96c
nh-g <giangngohong@gmail.com>
2021-04-14 16:47:15 +0200

    created postmapping to create a post

--- src/main/java/se/kth/sda/skeleton/posts/PostController.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostController.java
@@ -1,8 +1,29 @@
 package se.kth.sda.skeleton.posts;
 
-/*
-    @TODO create the methods needed to implement the API.
-    Don't forget to add necessary annotations.
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
 public class PostController {
+    PostRepository postRepository;
+
+    @Autowired
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    /* create a new post */
+    @PostMapping("/posts")
+    public ResponseEntity<Post> createPost(@Valid @RequestBody Post post) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(postRepository.save(post));
+    }
+
+
+
 }
