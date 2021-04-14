57fd7833a83a38f05a24deee9f00900ae113450c
decio <djnasty@gmail.com>
2021-04-14 18:20:54 +0100

    Added the gets, post, put & delete Post Control

--- src/main/java/se/kth/sda/skeleton/posts/PostController.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import se.kth.sda.skeleton.posts.exeption.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@RequestMapping("/posts")
 @RestController
 public class PostController {
     PostRepository postRepository;
@@ -17,16 +19,40 @@ public class PostController {
         this.postRepository = postRepository;
     }
     /* create a new post */
-    @PostMapping("/posts")
+    @PostMapping("/")
     public ResponseEntity<Post> createPost(@Valid @RequestBody Post post) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(postRepository.save(post));
     }
 
     /* return all posts*/
-    @GetMapping("/posts")
+    @GetMapping("/")
     public ResponseEntity <List<Post>>listAllPost() {
             return ResponseEntity.ok(postRepository.findAll());
+    }
+
+    /* return a specific post based on the provided id */
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id){
+        Post post = postRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        return ResponseEntity.ok(post);
+    }
+
+    /* return should update the post by id */
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePostById(@PathVariable Long id, @RequestBody Post updatePostById){
+        postRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        updatePostById.setId(id);
+        Post post = postRepository.save(updatePostById);
+        return ResponseEntity.ok(post);
+    }
+
+    /* should delete the post by id */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Post> deletePostById(@PathVariable Long id){
+        Post post = postRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        postRepository.delete(post);
+        return ResponseEntity.ok(post);
     }
 
 }
