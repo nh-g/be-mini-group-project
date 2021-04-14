515d1403e72e1eb4879ca97edd2035148c37492d
Fiona <thompsonfiona@outlook.com>
2021-04-14 19:06:59 +0200

    Added updatePost method to PostService

--- src/main/java/se/kth/sda/skeleton/posts/PostService.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostService.java
@@ -1,10 +1,26 @@
 package se.kth.sda.skeleton.posts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
-/*
-    @TODO Implement service methods.
- */
+@Service
 public class PostService {
+
+    PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post updatePost(Long id, Post updatedPost) {
+        postRepository.findById(id);
+        updatedPost.setId(id);
+        Post post = postRepository.save(updatedPost);
+        return post;
+    }
+
 }
