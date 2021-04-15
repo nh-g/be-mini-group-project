0bb024bb1466058262bcacc2e57d78b8a50a87b1
clementcardona <clecardona@gmail.com>
2021-04-15 10:52:19 +0200

    setup of CommentRepository setup of Comment model connexion to Post model

--- src/main/java/se/kth/sda/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda/skeleton/posts/Post.java
@@ -1,14 +1,33 @@
 package se.kth.sda.skeleton.posts;
 
-// @TODO add Hibernate annotations
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import se.kth.sda.skeleton.comments.Comment;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(nullable = false)
     private String body;
 
+    @OneToMany(mappedBy = "post")
+    List<Comment> comments;
+
+    // constructor:
     public Post() {
     }
 
+    // getters and setters:
     public Post(String body) {
         this.body = body;
     }
@@ -29,4 +48,11 @@ public class Post {
         this.body = body;
     }
 
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
