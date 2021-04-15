fd4bc2b86382484ae0d0fdb642d8f4219512e9b5
Fiona <thompsonfiona@outlook.com>
2021-04-15 15:28:54 +0200

    Add JavaDocs comments on PostController

--- src/main/java/se/kth/sda/skeleton/posts/PostController.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostController.java
@@ -19,34 +19,54 @@ public class PostController {
         this.postRepository = postRepository;
         this.postService = postService;
     }
-    /* create a new post */
+
+    /**
+     * Create a new post
+     * @param post a new user-created post
+     * @return HTTP created status of post
+     */
     @PostMapping("")
     public ResponseEntity<Post> createPost(@Valid @RequestBody Post post) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(postService.createPost(post));
     }
 
-    /* return all posts*/
+    /**
+     * Return a list of all posts
+     * @return HTTP ok status of listing all posts
+     */
     @GetMapping("")
     public ResponseEntity <List<Post>>listAllPosts() {
         return ResponseEntity.ok(postService.listAllPosts());
     }
 
-    /* return a specific post based on the provided id */
+    /**
+     * Return a specific post based on ID
+     * @param id the id of the specific post
+     * @return HTTP ok status of displaying post
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Post> getPostById(@PathVariable Long id){
         Post post = postService.getPostById(id);
         return ResponseEntity.ok(post);
     }
 
-    /* return should update the post by id */
+    /**
+     * Update a post based on ID
+     * @param id ID of the post to edit
+     * @param updatePostById new user-created body to update the post
+     * @return HTTP ok status of updated post
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post updatePostById){
         Post post = postService.updatePost(id, updatePostById);
         return ResponseEntity.ok(post);
     }
 
-    /* should delete the post by id */
+    /**
+     * Delete a post based on ID
+     * @param id ID of the post to delete
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePost(@PathVariable Long id){
