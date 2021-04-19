42f25eb34c42e4fa325d74cb011833baa39db0b0
nh-g <giangngohong@gmail.com>
2021-04-19 20:39:50 +0200

    fix text align

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -9,28 +9,33 @@ export default function HomePage() {
   return (
     <div className="card">
       <div className="card-body">
-        
-        <img className="logo"src={logo}/>
+        <img className="logo" src={logo} />
         <p>
-         Welcome to <em>Dumpty</em> a platform for Garbage trucks true lovers.<br/>
-         Welcome to the world of upCycling.<br/>
-         Here you will find the biggest fanbase .And don't forget : <br/> 
-         <em>In Crap We Trust</em>
-
-
+          Welcome to <em>Dumpty</em> a platform for Garbage Trucks true lovers.
+          <br />
+          <br />
+          Here you will find the biggest fanbase. <br />
+          And don't forget : <br />
+          <em>In Crap We Trust</em>
         </p>
         <ul>
           <li>
-            <a target="blank" href="https://store.steampowered.com/app/294830/RECYCLE/">Play the Game</a>
+            <a
+              target="blank"
+              href="https://store.steampowered.com/app/294830/RECYCLE/"
+            >
+              Game
+            </a>
           </li>
           <li>
-            <a target="blank" href="https://en.wikipedia.org/wiki/Reference">References </a>
+            <a target="blank" href="https://en.wikipedia.org/wiki/Reference">
+              References{" "}
+            </a>
           </li>
-       
         </ul>
       </div>
-      
-      <img className="illustration"src={truck}/>
+
+      <img className="illustration" src={truck} />
     </div>
   );
 }
