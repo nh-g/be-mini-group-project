2ecd0b95c923e7f9c52890fe2ecb66cd2067b8ca
nh-g <giangngohong@gmail.com>
2021-04-19 14:23:09 +0200

    refactor Post Card, wrap the whole post in a link, move delete button

--- frontend/src/pages/posts/Card.jsx
+++ frontend/src/pages/posts/Card.jsx
@@ -6,29 +6,27 @@ import userthumb from '../../assets/userthumb.png';
 
 export default function PostCard({ post, onDeleteClick }) {
 console.log(post);
-  return (
-    <div className="card mt-3">
-      <div className="card-body">
-
-      <div className="post-container">
-      <div className="post-header"> 
-
-      <p> <em>Jhon Doe</em> </p>
-      <img className="user-thumb" src={userthumb} />
-
-      </div>
-        <p className="post-body">{post.body}</p>
-
-        <button className="btn btn-danger" onClick={onDeleteClick}>
-          Delete Post
-        </button>
-
-              <Link to={{pathname:`/posts/${post.id}`, state: {post}}}>View Details</Link>
-
+return (
+  <div>
+    <Link to={{ pathname: `/posts/${post.id}`, state: { post } }}>
+      <div className="card mt-3">
+        <div className="card-body">
+          <div className="post-container">
+            <div className="post-header">
+              <p>
+                {" "}
+                <em>Jhon Doe</em>{" "}
+              </p>
+              <img className="user-thumb" src={userthumb} />
+            </div>
+            <p className="post-body">{post.body}</p>
+          </div>
         </div>
-
-
       </div>
-    </div>
-  );
+    </Link>
+    <button className="btn btn-danger" onClick={onDeleteClick}>
+      Delete Post
+    </button>
+  </div>
+);
 }
